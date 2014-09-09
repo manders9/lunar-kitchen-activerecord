da3b1435ad5809b484834c0592c049b92e014667
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-09-09 13:46:01 -0400

    all files added.

---
+++ models/ingredient.rb
@@ -0,0 +1,32 @@
+require 'pry'
+class Ingredient
+  attr_reader :name
+
+  def initialize(ingredient)
+
+
+
+    # ingredients = []
+    # id = params[:id]
+    # connection = PG.connect(dbname: 'recipes_')
+    #   results = connection.exec('SELECT * FROM ingredients WHERE recipe_id = $1', [id])
+    #   connection.close
+    #   results.each do |ingredient|
+    #   ingredients << Ingredient.new(ingredient)
+    # end
+    # recipes
+    @name = ingredient["name"]
+  end
+
+  def self.find(id)
+    ingredients = []
+
+    connection = PG.connect(dbname: 'recipes_')
+      results = connection.exec('SELECT * FROM ingredients WHERE recipe_id = $1', [id])
+    connection.close
+      results.each do |ingredient|
+      ingredients << Ingredient.new(ingredient)
+    end
+    ingredients
+  end
+end
